6ce1db69fee5728d9575ae71eda027c6a3161d19
Emily Wilder <emilywilder.earth@gmail.com>
2015-02-20 15:42:18 -0500

    make this readable...

--- ps2svg.sh
+++ ps2svg.sh
@@ -1,1 +1,14 @@
-ls | while read a ; do echo -n "$a..." ; if ! ps2pdf -dEPSCrop "$a" &>/dev/null ; then echo "ps2pdf $a" >> failed.log ; echo "failed" ; else ; if ! pdf2svg "${a%%.eps}.pdf" "${a%%.eps}.svg" ; then echo "pdf2svg $a" >> failed.log ; echo "failed" ; else ; echo "OK" ; fi ; fi ; done
+ls | while read a ; do
+    echo -n "$a..."
+    if ! ps2pdf -dEPSCrop "$a" &>/dev/null ; then
+        echo "ps2pdf $a" >> failed.log
+        echo "failed"
+    else
+        if ! pdf2svg "${a%%.eps}.pdf" "${a%%.eps}.svg" ; then
+            echo "pdf2svg $a" >> failed.log
+            echo "failed"
+        else
+            echo "OK"
+        fi
+    fi
+done
